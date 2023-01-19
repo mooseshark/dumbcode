6e6bfb95a2106857aae401ec0083de762a7457ca
mooseshark <moosesharkgames@gmail.com>
2023-01-18 21:00:41 -0500

    Magnitude Changes and Reset button

--- posts/bubble-wrap/bubble-wrap.js
+++ posts/bubble-wrap/bubble-wrap.js
@@ -1,6 +1,6 @@
 document.onreadystatechange = function () {
   if (document.readyState == "complete") {
-    generateSheet('blue','round-button-blue');
+    generateSheet('traditional','round-button-traditional');
   }
 };
 
@@ -9,12 +9,13 @@ document.onreadystatechange = function () {
 */
 function selectTheme(audioTheme) {
 	let styleTheme = document.getElementById(audioTheme).getAttribute('styletheme')
-	let selectedTheme = audioTheme || 'blue';
+	let selectedTheme = audioTheme || 'traditional';
 
 	resetPage(selectedTheme,styleTheme);
 }
 
 function generateSheet(audioTheme, styleTheme) {
+
 	for (let i = 0;i < 101;i++){
 		let btn = document.createElement('button');
 		btn.setAttribute('type', 'button');
@@ -38,18 +39,15 @@ function popBubble() {
 	let audio = new Audio(`audio/${this.getAttribute('audiotheme')}`);
 
 	audio.play();
-	this.setAttribute('class', 'round-button-popped');
+
+	theme = this.getAttribute('class');
+
+	this.setAttribute('class', `${theme}-popped`);
 	this.removeEventListener('click', popBubble);
 }
 
 function resetPage(selectedTheme,styleTheme){
-
-	
-	console.log(selectedTheme);
-	console.log(styleTheme);
-
 	if (!selectedTheme || !styleTheme){
-		console.log('in if');
 
 		let themeButtonToggleGroup = document.getElementById("button-toggle-group").children;
 
@@ -59,17 +57,12 @@ function resetPage(selectedTheme,styleTheme){
 				break;
 			}
 		}
-		selectedTheme = el.getAttribute('id');
-		styleTheme = el.getAttribute('audiotheme');
 
-		console.log('el');
-		console.log(el);
-		console.log(selectedTheme);
-		console.log(styleTheme);
+		selectedTheme = el.childNodes[1].getAttribute('id');
+		styleTheme = el.childNodes[1].getAttribute('styletheme');
 	}
 
-
-
+	
 	let childrenLen = document.getElementById("bubble-wrap").children.length
 
 	for(let i = 0; i <= childrenLen; i++){
@@ -78,6 +71,7 @@ function resetPage(selectedTheme,styleTheme){
 		if (bubble)
     		bubble.parentNode.removeChild(bubble);
 	}
+
 	if (document.getElementById("bubble-wrap").children.length === 0)
 		generateSheet(selectedTheme,styleTheme)
 }
@@ -86,12 +80,17 @@ function resetPage(selectedTheme,styleTheme){
  * Handle Audio
 */
 function assignAudio(theme) {
+	const traditional = ['bubble_crack_pop_2.mp3'];
 	const blue = ['bubble_cork_1.mp3','bubble_cork_2.mp3','bubble_crack_pop_1.mp3','bubble_crack_pop_2.mp3','bubble_double_drip_1.mp3','bubble_light_pop_1.mp3','bubble_light_pop_2.mp3','bubble_light_pop_3.mp3','bubble_pop_1.mp3','bubble_pop_2.mp3','bubble_pop_3.mp3','bubble_pop_woosh_1.mp3'];
 	const cat = ['kitten_meow_1.mp3','kitten_meow_2.mp3','kitten_meow_3.mp3','long_meow_1.mp3','meow_1.mp3','meow_2.mp3','meow_3.mp3','meow_4.mp3','meow_5.mp3','meow_6.mp3','meow_7.mp3','meow_angry_1.mp3','meow_angry_2.mp3','meow_angry_3.mp3','perfect_meow_1.mp3','purr_meow_1.mp3','purr_meow_2.mp3','purr_meow_3.mp3'];
 	const purple = blue.concat(cat);
+	const magnitude = ['magnitude_pop_pop.mp3']
 	let audioTheme;
 
 	switch(theme){
+		case 'traditional':
+			audioTheme = traditional;
+			break;
 		case 'blue':
 			audioTheme = blue;
 			break;
@@ -101,8 +100,11 @@ function assignAudio(theme) {
 		case 'purple':
 			audioTheme = purple;
 			break;
+		case 'magnitude':
+			audioTheme = magnitude;
+			break;
 		default:
-			audioTheme = blue;
+			audioTheme = traditional;
 			break;
 	}
 
